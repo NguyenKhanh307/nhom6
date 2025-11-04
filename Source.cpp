62e768f7de80c8f41c24eb361972066e408abe99
TranNguyenDat <trannguyendat2003@gmail.com>
2025-11-04 11:04:55 +0700

    ham_Xuat_N_So_Chan

--- Source.cpp
+++ Source.cpp
@@ -5,10 +5,24 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+void xuatnsochan(int n);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-	cout<<"Tong =";
+	cout<<"Nhap n: ";
+	cin>> n;
+	cout<<xuatnsochan(n);
 	system("pause");
 	return 0;
+}
+void xuatnsochan(int n){
+	int i=0;
+	int rs=0;
+	while(rs<n){
+		if(i%2==0){
+			cout<<i " ";
+			rs++;
+		}
+		i++;
+	}
 }
