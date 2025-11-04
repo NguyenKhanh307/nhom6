b6d76f698770d5c94da7cce72b214d53b62aca20
NguyenKhanh <khanh.nguyenphanhuu@gmail.com>
2025-11-04 11:02:45 +0700

    ham kiemtrasnt

--- Source.cpp
+++ Source.cpp
@@ -5,18 +5,20 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
-
+int kiemTraSNT(int a);
 int main()
 {
 	
-	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT"<<endl;
 	int a,b;
 	cout<<"Nhap a :";
 	cin>>a;
 	cout<<" Nhap b ";
 	cin>>b;
-	cout<<"Hieu ="<<hieu(a,b);
-	cout<<"Tong ="<<tong(a,b);
+	cout<<"Hieu ="<<hieu(a,b)<<endl;
+	cout<<"Tong ="<<tong(a,b)<<endl;
+	if(kiemTraSNT(a)) cout<<"a la so nguyen to"<<endl;
+	else "a khong la so nguyen to"<<endl;
 	system("pause");
 	return 0;
 }
@@ -26,4 +28,9 @@ int hieu(int a, int b){
 int tong(int a, int b){
 	return a+b;
 };
-
+int kiemTraSNT(int a){
+	for(int i=0;i<=sqrt(a);i++){
+		if(a%i==0) return false;
+	}
+	return true;
+}
